0486085c90dea0861ed2dc172439ef6cf9a9a8b4
Yeasin Arafat Nayem <87275236+yanayem@users.noreply.github.com>
2025-08-23 00:03:02 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Page/Home';
-import NotFoundPage from './Page/NotFoundPage';
+import Home from '.page/Home';
+import NotFoundPage from './page/NotFoundPage';
 import { Toaster } from 'react-hot-toast';
+
 
 const App = () => {
   useEffect(() => {
-    document.title = "Portfolio | Nayem";
+    document.title = "Portfolio";
   }, []); 
 
   return (
