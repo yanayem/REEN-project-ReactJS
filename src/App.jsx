562dd080bc61e5889d782853c3d970a8cd5e53ea
Yeasin Arafat Nayem <87275236+yanayem@users.noreply.github.com>
2025-08-22 23:42:42 +0600

    Update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './page/Home';
-import NotFoundPage from './page/NotFoundPage';
+import Home from './Page/Home';
+import NotFoundPage from './Page/NotFoundPage';
 import { Toaster } from 'react-hot-toast';
 
 const App = () => {
